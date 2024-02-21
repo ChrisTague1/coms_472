c15c3b311baf64ecc3437f42f36c9d7cbd6e2a7e
Chris Tague <cstague6401@gmail.com>
2024-02-21 17:47:01 -0600

    done

--- runner.py
+++ runner.py
@@ -11,20 +11,23 @@ nodes = []
 times = []
 nodes = []
 
-for filename in os.listdir(directory):
-    file_path = os.path.join(directory, filename)
-    command = ['python', 'main.py', file_path, alg]
-    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
-    output_json = json.loads(result.stdout)
-    times.append(output_json['time'])
-    nodes.append(output_json['nodes'])
+# for filename in os.listdir(directory):
+#     file_path = os.path.join(directory, filename)
+#     command = ['python', 'main.py', file_path, alg]
+#     result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
+#     output_json = json.loads(result.stdout)
+#     times.append(output_json['time'])
+#     nodes.append(output_json['nodes'])
+#
+
+with open('ids_output.txt', 'r') as file:
+    for line in file:
+        data = json.loads(line)
+        times.append(data['time'])
+        nodes.append(data['nodes'])
 
 avg_time = sum(times) / len(times) if times else 0
 avg_nodes = sum(nodes) / len(nodes) if nodes else 0
-
-with open('the_output.txt', 'a') as file:
-    file.write(f"Average Time: {avg_time}")
-    file.write(f"Average Nodes: {avg_nodes}")
 
 print("Average Time:", avg_time)
 print("Average Nodes:", avg_nodes)
